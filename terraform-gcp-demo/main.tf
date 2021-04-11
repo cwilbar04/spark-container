9d0f740329e942fc31fd7c9db034404d9feade4e
Christopher Wilbar <cwilbar@alumni.nd.edu>
2021-04-11 12:29:14 -0500

    terraform testing

---
+++ terraform-gcp-demo/main.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "3.5.0"
+    }
+  }
+}
+
+provider "google" {
+
+  credentials = file("C:/GCP/spark-container-dev-f5d53ab2439c.json")
+
+  project = "spark-container-dev"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
